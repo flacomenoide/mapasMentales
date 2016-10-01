ddc3add6c4dad9e9120fd9d4f03fed0166591369
flacomenoide <miguel.jinez@gmail.com>
2016-10-01 00:14:36 -0300

    R: Fundamentos - Funciones matemáticas

--- lenguajesProgramacion/codigoR/fundamentos.R
+++ lenguajesProgramacion/codigoR/fundamentos.R
@@ -9,6 +9,24 @@ x <- otra_Variable
 #   - Diferencian entre mayúsculas y minúsculas
 #   - No pueden ser solamente números
 #   - No pueden contener caracteres especiales como ! - * &
- g <- 5
- g+x
- 
+g <- 5
+
+# Operaciones Matemáticas
+g+x           # Suma
+g-x           # Resta
+g*x           # Multiplicación
+x/g           # División
+sqrt(x)       # Raíz cuadrada
+x^g           # x elevado a la potencia g
+exp(g)        # e elevado a la potencia g
+log(g, x)     # Logaritmo de g en base x
+factorial(g)  # Factorial de g
+cos(x)        # Coseno de x
+sin(x)        # Seno de x
+tan(x)        # Tangente de x
+abs(x)        # Valor absoluto de x
+
+z <- cbind(x1 = 3, x2 = c(4:1, 2:5)) # Se crae un data frame con 2 columnas y 8 filas
+rowMeans(z)   # Cálculo de los promedios de todas las filas
+colSums(z)    # Cálculo de la suma de todas las filas del data frame z
+rowSums(z)    # Cálculo de la suma de todas las columnas del data frame z
