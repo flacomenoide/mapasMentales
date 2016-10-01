f044ab85bf7f8535b8d536df9ce9971f9a522fdb
flacomenoide <miguel.jinez@gmail.com>
2016-09-30 22:02:05 -0300

    R: Fundamentos

--- lenguajesProgramacion/codigoR/fundamentos.R
+++ lenguajesProgramacion/codigoR/fundamentos.R
@@ -1,2 +1,14 @@
 # Fundamentos de R
-print("Hola Mundo")
+
+# El operador <- Sirve para asignar valores a variables
+variable <- "valor" # Asignación de un valores a variables
+otra_Variable <- 10
+x <- otra_Variable
+
+# Los nombres de las variables:
+#   - Diferencian entre mayúsculas y minúsculas
+#   - No pueden ser solamente números
+#   - No pueden contener caracteres especiales como ! - * &
+ g <- 5
+ g+x
+ 
