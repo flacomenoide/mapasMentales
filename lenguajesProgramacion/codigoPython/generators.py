430f2ca19137c67433185fe8af040c99e4856801
flacomenoide <miguel.jinez@gmail.com>
2016-11-03 23:48:34 -0300

    Python: Generetors - Expressions

--- lenguajesProgramacion/codigoPython/generators.py
+++ lenguajesProgramacion/codigoPython/generators.py
@@ -1,6 +1,8 @@
 # Uso de Generators
 # creacion de una funcion que obtenga los numeros pares
 
+# Generator Function
+# ==================
 def generator_numeros_pares(n):
     for i in range(n):
         if i % 2 == 0:
@@ -8,4 +10,14 @@ def generator_numeros_pares(n):
 
 # La invocacion de una funcion generator da como resultado un generator object
 print generator_numeros_pares(6)
-print list(generator_numeros_pares(6))
+print list(generator_numeros_pares(9))
+
+
+# Generator Expression
+# ====================
+(i for i in range(1,9))
+list(i for i in range(1,9))
+
+pares = (n for n in range(10) if n%2==0)
+print pares
+print list(pares)
