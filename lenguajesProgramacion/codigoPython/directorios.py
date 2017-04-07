057c94d0d71f8c39f13c7fdb56d297db0feebc02
flacomenoide <miguel.jinez@gmail.com>
2017-04-06 23:09:54 -0300

    Python: Recorrer directorios y su contenido - Uso de expresiones regulares

---
+++ lenguajesProgramacion/codigoPython/directorios.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Recorrer elementos de un directorio
+
+"""
+
+"""
+Recorre un todos los archivos de un directorio e imprime sus archivos
+"""
+def imprime_archivos(directorio):
+    for directorio, subDirectorios, archivos in os.walk(directorio):
+        for archivo in archivos:
+            print(archivo)
+
+
+"""
+Recorre un todos los archivos de un directorio e imprime sus archivos con el
+patrón *.vym
+"""
+def imprime_archivos_con_patron(directorio):    
+    for directorio, subDirectorios, archivos in os.walk(directorio):
+        for archivo in archivos:
+            if re.search("(.+)\.(vym)$", archivo):
+                print(archivo)
+
+
+import os, re
+
+aFolder = "/home/mike/mapasMentales"
+imprime_archivos(aFolder)
+imprime_archivos_con_patron(aFolder)
