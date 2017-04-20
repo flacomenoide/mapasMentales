6528215412ac902e400fd760f52d2fbb17a33faa
flacomenoide <miguel.jinez@gmail.com>
2017-04-19 22:29:21 -0300

    Blockchain: Ajustes a mapa mental

--- blockchain/codigo_python/herramientas_bc.py
+++ blockchain/codigo_python/herramientas_bc.py
@@ -13,12 +13,15 @@ def uint1(stream):
     return ord(stream.read(1))
 
 def uint2(stream):
+    # H - short int sin signo
     return struct.unpack('H', stream.read(2))[0]
 
 def uint4(stream):
+    # I - int sin signo
     return struct.unpack('I', stream.read(4))[0]
 
 def uint8(stream):
+    # Q - long long sin signo
     return struct.unpack('Q', stream.read(8))[0]
 
 def hash32(stream):
@@ -29,7 +32,7 @@ def time(stream):
     return time
 
 def varint(stream):
-    size 0 uint1(stream)
+    size = uint1(stream)
     if size < 0xfd:
         return size
     if size == 0xfd:
